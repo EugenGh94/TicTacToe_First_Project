ceb2d35becdb54ab532be636bf8adcbdabadf01a
Eugen Gheorghe <eugengheorghe.med@gmail.com>
2025-07-09 21:17:17 +0300

    Update1_TicTacToe_First_Project.py
    I made a few changes in order to add more features to the game to make it work better.
    I added a 'While' loop for p1 and p2 functions, to check for wrong input from the user, I also made a new index() function to not allow a player to use a space that was already used.
    Finally I added a new condition for 'won()' function, to check if there is a stalemate between players.

--- TicTacToe_First_Project.py
+++ TicTacToe_First_Project.py
@@ -1,5 +1,5 @@
 area = ('   ', '   ', '   ')
-p1_line,p1_row,p2_line,p2_row = '','','','' 
+p1_line,p1_column,p2_line,p2_column = '','','','' 
 
 def start():
     trigger = True
@@ -15,80 +15,110 @@ def start():
     print('\n')
 
 def p1():
-    global area
-    print('X to play:')
-    p1_line = int(input())
-    p1_row = int(input())
-    new = area[p1_line-1][:p1_row-1] + 'X' + area[p1_line-1][p1_row:]
-    y = list(area)
-    y[p1_line-1] = new
-    area = tuple(y)
-    trigger = True
-    t = 1
-    for _ in area:
-        if trigger:
-            print('  1 2 3')
-            trigger = False
-        print(f'{t} {_[0]}|{_[1]}|{_[2]}')
-        t += 1
-        if t <= 3:
-            print('  -+-+-')
+    global area,p1_line,p1_column
+    def input_p1():
+        global area,p1_line,p1_column
+        print('X to play:')
+        p1_line = input('Line: ')
+        while p1_line not in (str(1),str(2),str(3)):
+            print('\nOnly 1, 2 or 3 allowed, try again!\n\nX to play:')
+            p1_line = input('Line: ')
+        p1_line = int(p1_line)
+        p1_column = input('Column: ')
+        while p1_column not in (str(1),str(2),str(3)):
+            print('\nOnly 1, 2 or 3 allowed, try again!\n\nX to play:')
+            p1_column = input('Column: ')
+        p1_column = int(p1_column)
+    def index_p1():
+        global area,p1_line,p1_column
+        new = area[p1_line-1][:p1_column-1] + 'X' + area[p1_line-1][p1_column:]
+        y = list(area)
+        y[p1_line-1] = new
+        area = tuple(y)
+        start()
+    input_p1()
+    while area[p1_line-1][p1_column-1] != ' ':
+        print('-'*28 + '\n' 'This space was already used!\n' + '-'*28)
+        input_p1()
+    else:
+        index_p1()
 
 def p2():
-    global area
-    print('0 to play:')
-    p2_line = int(input())
-    p2_row = int(input())
-    new = area[p2_line-1][:p2_row-1] + '0' + area[p2_line-1][p2_row:]
-    y = list(area)
-    y[p2_line-1] = new
-    area = tuple(y)
-    trigger = True
-    t = 1
-    for _ in area:
-        if trigger:
-            print('  1 2 3')
-            trigger = False
-        print(f'{t} {_[0]}|{_[1]}|{_[2]}')
-        t += 1
-        if t <= 3:
-            print('  -+-+-')
+    global area,p2_line,p2_column
+    def input_p2():
+        global area,p2_line,p2_column
+        print('0 to play:')
+        p2_line = input('Line: ')
+        while p2_line not in (str(1),str(2),str(3)):
+            print('\nOnly 1, 2 or 3 allowed, try again!\n\n0 to play:')
+            p2_line = input('Line: ')
+        p2_line = int(p2_line)
+        p2_column = input('Column: ')
+        while p2_column not in (str(1),str(2),str(3)):
+            print('\nOnly 1, 2 or 3 allowed, try again!\n\n0 to play:')
+            p2_column = input('Column: ')
+        p2_column = int(p2_column)
+    def index_p2():
+        global area,p2_line,p2_column
+        new = area[p2_line-1][:p2_column-1] + '0' + area[p2_line-1][p2_column:]
+        y = list(area)
+        y[p2_line-1] = new
+        area = tuple(y)
+        start()
+    input_p2()
+    while area[p2_line-1][p2_column-1] != ' ':
+        print('-'*28 + '\n' 'This space was already used!\n' + '-'*28)
+        input_p2()
+    else:
+        index_p2()
 
 def won():
-    if ((area[0][0]=='X' and area[0][1]=='X' and area[0][2]=='X') or
-        (area[1][0]=='X' and area[1][1]=='X' and area[1][2]=='X') or
-        (area[2][0]=='X' and area[2][1]=='X' and area[2][2]=='X') or
-        (area[0][0]=='X' and area[1][0]=='X' and area[2][0]=='X') or
-        (area[0][1]=='X' and area[1][1]=='X' and area[2][1]=='X') or
-        (area[0][2]=='X' and area[1][2]=='X' and area[2][2]=='X') or
-        (area[0][0]=='X' and area[1][1]=='X' and area[2][2]=='X') or
-        (area[0][2]=='X' and area[1][1]=='X' and area[2][0]=='X') or
-        
-        (area[0][0]=='0' and area[0][1]=='0' and area[0][2]=='0') or
-        (area[1][0]=='0' and area[1][1]=='0' and area[1][2]=='0') or
-        (area[2][0]=='0' and area[2][1]=='0' and area[2][2]=='0') or
-        (area[0][0]=='0' and area[1][0]=='0' and area[2][0]=='0') or
-        (area[0][1]=='0' and area[1][1]=='0' and area[2][1]=='0') or
-        (area[0][2]=='0' and area[1][2]=='0' and area[2][2]=='0') or
-        (area[0][0]=='0' and area[1][1]=='0' and area[2][2]=='0') or
-        (area[0][2]=='0' and area[1][1]=='0' and area[2][0]=='0')):
-        return 'WINNER'
+    global area
+    if ' ' in area[0] or ' ' in area[1] or ' ' in area[2]:
+        if ((area[0][0]=='X' and area[0][1]=='X' and area[0][2]=='X') or
+            (area[1][0]=='X' and area[1][1]=='X' and area[1][2]=='X') or
+            (area[2][0]=='X' and area[2][1]=='X' and area[2][2]=='X') or
+            (area[0][0]=='X' and area[1][0]=='X' and area[2][0]=='X') or
+            (area[0][1]=='X' and area[1][1]=='X' and area[2][1]=='X') or
+            (area[0][2]=='X' and area[1][2]=='X' and area[2][2]=='X') or
+            (area[0][0]=='X' and area[1][1]=='X' and area[2][2]=='X') or
+            (area[0][2]=='X' and area[1][1]=='X' and area[2][0]=='X') or
+            
+            (area[0][0]=='0' and area[0][1]=='0' and area[0][2]=='0') or
+            (area[1][0]=='0' and area[1][1]=='0' and area[1][2]=='0') or
+            (area[2][0]=='0' and area[2][1]=='0' and area[2][2]=='0') or
+            (area[0][0]=='0' and area[1][0]=='0' and area[2][0]=='0') or
+            (area[0][1]=='0' and area[1][1]=='0' and area[2][1]=='0') or
+            (area[0][2]=='0' and area[1][2]=='0' and area[2][2]=='0') or
+            (area[0][0]=='0' and area[1][1]=='0' and area[2][2]=='0') or
+            (area[0][2]=='0' and area[1][1]=='0' and area[2][0]=='0')):
+            return 'A'
+    else:
+        return 'B'
 
 def run_game():
+    trigger = False
     start()
-    p1()
-    while won() != 'WINNER':
-        p2()
-        while won() != 'WINNER':
-            p1()
+    while trigger == False:
+        if won() == 'A':
+            print('-'*16 + '\n' '0 is the Winner!\n' + '-'*16)
             break
-        else:
-            print('0 is the Winner !')
-            return 'end'
-    else:
-        print('X is the Winner !')
-        return 'end'
-    
-    
+        elif won() == 'B':    
+            print('-'*17 + '\n' +'S T A L E M A T E\n' + '-'*17)
+            break
+        else:    
+            p1()
+            trigger = True
+        while trigger == True:
+            if won() == 'A':
+                print('-'*16 + '\n' 'X is the Winner!\n' + '-'*16)
+                break
+            elif won() == 'B':    
+                print('-'*17 + '\n' +'S T A L E M A T E\n' + '-'*17)
+                break
+            else:    
+                p2()
+                trigger = False
+                break
 
 run_game()
