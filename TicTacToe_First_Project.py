c045b8a28a0bfe4619d4133f7c93bbb04119a60a
Eugen Gheorghe <eugengheorghe.med@gmail.com>
2025-07-10 14:39:02 +0300

    Update2 TicTacToe_First_Project.py
    In the final iteration I changed and added a few more things that were missing in order for the game to run perfectly. 
    I changed the stalemate condition to check after checking for winners because I was getting a 'STALEMATE' ending when a player was winning the game with the last space available. 
    I also made the player function a bit more complex by using variable-variable names in order to have one function that works for both players. 
    Finally, I made the game replayable at the end and added docstrings to explain what everything does.

--- TicTacToe_First_Project.py
+++ TicTacToe_First_Project.py
@@ -1,102 +1,100 @@
-area = ('   ', '   ', '   ')
-p1_line,p1_column,p2_line,p2_column = '','','','' 
+area = ['   ', '   ', '   ']
+p1_line,p1_column,p2_line,p2_column = '','','',''
+
 
 def start():
+    #Prints game area
     trigger = True
     t = 1
     for _ in area:
-        if trigger:
-            print('  1 2 3')
-            trigger = False
+        if trigger: print('  1 2 3'); trigger = False
         print(f'{t} {_[0]}|{_[1]}|{_[2]}')
         t += 1
-        if t <= 3:
-            print('  -+-+-')
+        if t <= 3: print('  -+-+-')
     print('\n')
 
-def p1():
-    global area,p1_line,p1_column
-    def input_p1():
-        global area,p1_line,p1_column
-        print('X to play:')
-        p1_line = input('Line: ')
-        while p1_line not in (str(1),str(2),str(3)):
-            print('\nOnly 1, 2 or 3 allowed, try again!\n\nX to play:')
-            p1_line = input('Line: ')
-        p1_line = int(p1_line)
-        p1_column = input('Column: ')
-        while p1_column not in (str(1),str(2),str(3)):
-            print('\nOnly 1, 2 or 3 allowed, try again!\n\nX to play:')
-            p1_column = input('Column: ')
-        p1_column = int(p1_column)
-    def index_p1():
-        global area,p1_line,p1_column
-        new = area[p1_line-1][:p1_column-1] + 'X' + area[p1_line-1][p1_column:]
-        y = list(area)
-        y[p1_line-1] = new
-        area = tuple(y)
-        start()
-    input_p1()
-    while area[p1_line-1][p1_column-1] != ' ':
-        print('-'*28 + '\n' 'This space was already used!\n' + '-'*28)
-        input_p1()
-    else:
-        index_p1()
 
-def p2():
-    global area,p2_line,p2_column
-    def input_p2():
-        global area,p2_line,p2_column
-        print('0 to play:')
-        p2_line = input('Line: ')
-        while p2_line not in (str(1),str(2),str(3)):
-            print('\nOnly 1, 2 or 3 allowed, try again!\n\n0 to play:')
-            p2_line = input('Line: ')
-        p2_line = int(p2_line)
-        p2_column = input('Column: ')
-        while p2_column not in (str(1),str(2),str(3)):
-            print('\nOnly 1, 2 or 3 allowed, try again!\n\n0 to play:')
-            p2_column = input('Column: ')
-        p2_column = int(p2_column)
-    def index_p2():
-        global area,p2_line,p2_column
-        new = area[p2_line-1][:p2_column-1] + '0' + area[p2_line-1][p2_column:]
-        y = list(area)
-        y[p2_line-1] = new
-        area = tuple(y)
+def player(x):
+    
+    def input_player(x):
+        #Condition for Player 1('X') or Player 2('0')
+        if x == 'p1': p_type = 'X'
+        else: p_type = '0'
+        print(f'{p_type} to play:')
+        
+        #Prompts player for 'LINE' input and checks if valid / if not prompts again
+        line = input('Line: ')
+        while line not in (str(1),str(2),str(3)):
+            print(f'\nOnly 1, 2 or 3 allowed, try again!\n\n{p_type} to play:')
+            line = input('Line: ')
+        #Saves player's 'LINE' input in var (x)_line
+        globals()[f'{x}_line'] = int(line)
+        
+        #Prompts player for 'COLUMN' input and checks if valid / if not prompts again
+        column = input('Column: ')
+        while column not in (str(1),str(2),str(3)):
+            print(f'\nOnly 1, 2 or 3 allowed, try again!\n\n{p_type} to play:')
+            column = input('Column: ')
+        #Saves player's 'COLUMN' input in var (x)_column
+        globals()[f'{x}_column'] = int(column)
+    
+
+    def indexing(x):
+        global area
+        #Condition for Player 1('X') or Player 2('0')
+        if x == 'p1': p_type = 'X'
+        else: p_type = '0'
+        #Breaks string at player's inputs-1, adds 'X'/'0' and rejoins string
+        area[globals()[f'{x}_line']-1] = area[globals()[f'{x}_line']-1][:globals()[f'{x}_column']-1] \
+                                       + p_type \
+                                       + area[globals()[f'{x}_line']-1][globals()[f'{x}_column']:]
         start()
-    input_p2()
-    while area[p2_line-1][p2_column-1] != ' ':
+    
+    
+    input_player(x)
+    #Checks if player's input location is already used or not
+    while area[globals()[f'{x}_line']-1][globals()[f'{x}_column']-1] != ' ':
         print('-'*28 + '\n' 'This space was already used!\n' + '-'*28)
-        input_p2()
+        input_player(x)
     else:
-        index_p2()
+        indexing(x)
+
 
 def won():
     global area
-    if ' ' in area[0] or ' ' in area[1] or ' ' in area[2]:
-        if ((area[0][0]=='X' and area[0][1]=='X' and area[0][2]=='X') or
-            (area[1][0]=='X' and area[1][1]=='X' and area[1][2]=='X') or
-            (area[2][0]=='X' and area[2][1]=='X' and area[2][2]=='X') or
-            (area[0][0]=='X' and area[1][0]=='X' and area[2][0]=='X') or
-            (area[0][1]=='X' and area[1][1]=='X' and area[2][1]=='X') or
-            (area[0][2]=='X' and area[1][2]=='X' and area[2][2]=='X') or
-            (area[0][0]=='X' and area[1][1]=='X' and area[2][2]=='X') or
-            (area[0][2]=='X' and area[1][1]=='X' and area[2][0]=='X') or
+    	#Checks if there are 3 'X' in the same row'''
+    if ((area[0][0]=='X' and area[0][1]=='X' and area[0][2]=='X') or
+        (area[1][0]=='X' and area[1][1]=='X' and area[1][2]=='X') or
+        (area[2][0]=='X' and area[2][1]=='X' and area[2][2]=='X') or
+            #Checks if there are 3 'X' in the same column
+        (area[0][0]=='X' and area[1][0]=='X' and area[2][0]=='X') or
+        (area[0][1]=='X' and area[1][1]=='X' and area[2][1]=='X') or
+        (area[0][2]=='X' and area[1][2]=='X' and area[2][2]=='X') or
+                #Checks if there are 3 'X' in the same diagonal
+        (area[0][0]=='X' and area[1][1]=='X' and area[2][2]=='X') or
+        (area[0][2]=='X' and area[1][1]=='X' and area[2][0]=='X') or
             
-            (area[0][0]=='0' and area[0][1]=='0' and area[0][2]=='0') or
-            (area[1][0]=='0' and area[1][1]=='0' and area[1][2]=='0') or
-            (area[2][0]=='0' and area[2][1]=='0' and area[2][2]=='0') or
-            (area[0][0]=='0' and area[1][0]=='0' and area[2][0]=='0') or
-            (area[0][1]=='0' and area[1][1]=='0' and area[2][1]=='0') or
-            (area[0][2]=='0' and area[1][2]=='0' and area[2][2]=='0') or
-            (area[0][0]=='0' and area[1][1]=='0' and area[2][2]=='0') or
-            (area[0][2]=='0' and area[1][1]=='0' and area[2][0]=='0')):
-            return 'A'
-    else:
+        #Checks if there are 3 '0' in the same row
+        (area[0][0]=='0' and area[0][1]=='0' and area[0][2]=='0') or
+        (area[1][0]=='0' and area[1][1]=='0' and area[1][2]=='0') or
+        (area[2][0]=='0' and area[2][1]=='0' and area[2][2]=='0') or
+            #Checks if there are 3 '0' in the same column
+        (area[0][0]=='0' and area[1][0]=='0' and area[2][0]=='0') or
+        (area[0][1]=='0' and area[1][1]=='0' and area[2][1]=='0') or
+        (area[0][2]=='0' and area[1][2]=='0' and area[2][2]=='0') or
+                #Checks if there are 3 '0' in the same diagonal
+        (area[0][0]=='0' and area[1][1]=='0' and area[2][2]=='0') or
+        (area[0][2]=='0' and area[1][1]=='0' and area[2][0]=='0')):
+        return 'A'
+
+    #Checks if there are any free spaces left, if not displays STALEMATE
+    if ' ' not in area[0] and ' ' not in area[1] and ' ' not in area[2]:
         return 'B'
 
+
 def run_game():
+    global area,p1_line,p1_column,p2_line,p2_column
+    #Runs game and checks if any player won or if it's a stalemate
     trigger = False
     start()
     while trigger == False:
@@ -107,7 +105,7 @@ def run_game():
             print('-'*17 + '\n' +'S T A L E M A T E\n' + '-'*17)
             break
         else:    
-            p1()
+            player('p1')
             trigger = True
         while trigger == True:
             if won() == 'A':
@@ -117,8 +115,16 @@ def run_game():
                 print('-'*17 + '\n' +'S T A L E M A T E\n' + '-'*17)
                 break
             else:    
-                p2()
+                player('p2')
                 trigger = False
                 break
+    
+    #Ask user if they want to play again and reset game, else do nothing
+    temp = input('Play again?\nY (yes) / N (no)\n')
+    if temp.upper() == 'Y':
+        area = ['   ', '   ', '   ']
+        p1_line,p1_column,p2_line,p2_column = '','','',''
+        run_game()
+
 
 run_game()
